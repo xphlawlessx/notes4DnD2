3c6f4b7ecb0fa3e5a0f15050689ec8ad0330dbec
xphlawlessx <phalyce@googlemail.com>
2020-06-05 14:28:07 +0100

    Added tools, large refactor, 0.0.2 added - Dice roller, initiative tracker, soundboard. moved overlay code to components. (fixed changelist error)

--- notes/note-overlay.js
+++ notes/note-overlay.js
@@ -8,8 +8,8 @@ Vue.component('note-overlay', {
     template:
         `
         <span class="overlay-content">
-            <label for="title">Title:</label>
-        <input id="title" type="text" name="title" v-model="nameLabelString">
+            <label for="title" style="color: white">Title:</label>
+        <input style="color: white" id="title" type="text" name="title" v-model="nameLabelString">
         <textarea placeholder="Describe the encounter." id="text" name="text"
                   style="overflow-y: scroll; word-wrap: break-word; resize: none; font-size: 16px; height: 90%;width:140%"
                   v-model="noteBodyString"></textarea>
